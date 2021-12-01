1a284efbd7e33c747072f8c0447a9f8ff4e7805e
das_ <git@heap.mozmail.com>
2021-12-01 21:50:28 +0100

    Clean up Day 1 Clojure solution (#17)

--- Day-01/clojure/das_/day01.clj
+++ Day-01/clojure/das_/day01.clj
@@ -1,31 +1,28 @@
 (ns day01
   (:require [clojure.string :as str]))
 
-(defn read-numbers [input]
-  (map #(Integer/valueOf %) (str/split-lines input)))
+(defn read-numbers-from-file [file]
+  (->> file
+      (slurp)
+      (str/split-lines)
+      (map #(Integer/valueOf %))))
 
-(defn count-increasing-numbers [coll]
+(defn count-increasing-numbers [window coll]
   (->> coll
+       (partition window 1)
+       (map #(reduce + %))
        (partition-all 2 1)
        (drop-last)
        (filter #(> (second %) (first %)))
        (count)))
 
-(defn part-1 [input]
-  (->> input
-       (read-numbers)
-       (count-increasing-numbers)))
+(defn part-1 [input] (count-increasing-numbers 1 input))
 
-(defn part-2 [input]
-  (->> input
-       (read-numbers)
-       (partition 3 1)
-       (map #(reduce + %))
-       (count-increasing-numbers)))
-
+(defn part-2 [input] (count-increasing-numbers 3 input))
 
 (defn -main [& args]
   (if (not= (count args) 0)
-      (do (println "Part 1:" (part-1 (slurp (first args))))
-          (println "Part 2:" (part-2 (slurp (first args)))))
+      (let [input (read-numbers-from-file (first args))]
+        (println "Part 1:" (part-1 input))
+        (println "Part 2:" (part-2 input)))
       (println "Please enter the input file as an argument!")))
