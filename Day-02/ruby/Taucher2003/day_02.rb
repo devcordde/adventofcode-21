bd0c7db255de8b6d0c706876f42cfd4f446f659b
Niklas van Schrick <mc.taucher2003@gmail.com>
2021-12-02 12:34:58 +0100

    Fix formatting for day2/ruby

--- Day-02/ruby/Taucher2003/day_02.rb
+++ Day-02/ruby/Taucher2003/day_02.rb
@@ -4,20 +4,20 @@ lines = input.split("\n").map { |line| line.split(" ") }
 results = [[0, 0], [0, 0, 0]]
 
 lines.each do |line|
-	command = line[0]
-	value = line[1].to_i
+  command = line[0]
+  value = line[1].to_i
 	
-	# Part 1
-	results[0][0] += value if command == "forward"
-	results[0][1] += value if command == "down"
-	results[0][1] -= value if command == "up"
+  # Part 1
+  results[0][0] += value if command == "forward"
+  results[0][1] += value if command == "down"
+  results[0][1] -= value if command == "up"
 
-	# Part 2
-	if command == "forward"
-		results[1][0] += value
-		results[1][1] += results[1][2] * value 
-	end
-	results[1][2] += value if command == "down"
+  # Part 2
+  if command == "forward"
+    results[1][0] += value
+    results[1][1] += results[1][2] * value 
+  end
+  results[1][2] += value if command == "down"
   results[1][2] -= value if command == "up"
 end
 
