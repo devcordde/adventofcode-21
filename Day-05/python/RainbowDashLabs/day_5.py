f0cad885aa354de6dfac58c5ba7317c729756ee7
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2021-12-06 00:24:42 +0100

    Cleanup code (#61)

--- Day-05/python/RainbowDashLabs/day_5.py
+++ Day-05/python/RainbowDashLabs/day_5.py
@@ -1,3 +1,6 @@
+from typing import Callable, Union
+
+
 class Point:
     def __init__(self, x, y):
         self.x = int(x)
@@ -6,26 +9,26 @@ class Point:
     def __repr__(self):
         return f"{self.x},{self.y}"
 
-    def __sub__(self, other):
+    def __sub__(self, other: Union["Point", int, float]):
         if isinstance(other, Point):
             return Point(self.x - other.x, self.y - other.y)
         return Point(self.x - other, self.y - other)
 
-    def __add__(self, other):
+    def __add__(self, other: Union["Point", int, float]):
         if isinstance(other, Point):
             return Point(self.x + other.x, self.y + other.y)
         return Point(self.x + other, self.y + other)
 
-    def between(self, point):
-        direction = (point.x - self.x, point.y - self.y)
-        points = []
+    def __mul__(self, other: Union["Point", int, float]):
+        if isinstance(other, Point):
+            return Point(self.x * other.x, self.y * other.y)
+        return Point(self.x * other, self.y * other)
 
-        steps = (abs(direction[0]) / (direction[0] or 1), abs(direction[1]) / (direction[1] or 1))
-
-        for x in range(max((abs(direction[0]) or 1) + 1, (abs(direction[1]) or 1) + 1)):
-            points.append(Point(self.x + x * steps[0], self.y + x * steps[1]))
-
-        return points
+    def between(self, point: "Point"):
+        direction = point - self
+        steps = (abs(direction.x) / (direction.x or 1), abs(direction.y) / (direction.y or 1))
+        max_range = max((abs(direction.x)) + 1, (abs(direction.y)) + 1)
+        return [self + (Point(steps[0], steps[1]) * x) for x in range(max_range)]
 
     def is_diagonal(self, other):
         return not (self.x == other.x or self.y == other.y)
@@ -53,35 +56,38 @@ class Line:
         return self.start.is_diagonal(self.end)
 
 
+class Map:
+    def __init__(self):
+        self.coords = {}
+
+    def add(self, point: Point):
+        if point in self.coords:
+            self.coords[point] += 1
+        else:
+            self.coords[point] = 1
+
+    def add_all(self, lines: list[Line], predicate: Callable[[Line], bool] = lambda x: True):
+        for line in lines:
+            if not predicate(line):
+                continue
+            for point in line.points():
+                self.add(point)
+
+    def __iter__(self):
+        return self.coords.items().__iter__()
+
+    def count(self, predicate: Callable[[Point, int], bool]):
+        return len([v for k, v in self if predicate(k, v)])
+
+
 lines = [Line(e) for e in open("puzzle.txt") if e.strip()]
 
-for line in lines:
-    print(line)
+my_map = Map()
+my_map.add_all(lines, lambda point: not point.is_diagonal())
 
-points = {}
+print(f"Part one {my_map.count(lambda k, v: v > 1)}")
 
-for line in lines:
-    if line.is_diagonal():
-        continue
-    for point in line.points():
-        if point in points:
-            points[point] += 1
-        else:
-            points[point] = 1
+my_map = Map()
+my_map.add_all(lines)
 
-count = [(k, v) for k, v in points.items() if v > 1]
-
-print(f"Part one {len(count)}")
-
-points = {}
-
-for line in lines:
-    for point in line.points():
-        if point in points:
-            points[point] += 1
-        else:
-            points[point] = 1
-
-count = [(k, v) for k, v in points.items() if v > 1]
-
-print(f"Part two {len(count)}")
+print(f"Part two {my_map.count(lambda k, v: v > 1)}")
