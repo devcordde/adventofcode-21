c1ae7ab3b67dd919761d228d8fca04923ffe4b6c
das_ <git@heap.mozmail.com>
2021-12-08 15:48:51 +0100

    Add Clojure day 3 part 2 (#74)
    * Add Clojure day 3 part 2
    
    * Enhance naming

--- Day-03/clojure/das_/day03.clj
+++ Day-03/clojure/das_/day03.clj
@@ -12,19 +12,43 @@
 (defn get-column [index coll]
   (mapv #(get % (int index)) coll))
 
-(defn get-relevant-bits [index frequencies]
-  (mapv #(first (nth % index)) frequencies))
+(defn sorted-bit-frequencies [coll]
+  (mapv #(sort-by val (frequencies %)) (for [i (range (count (first coll)))] (get-column i coll))))
+
+(defn find-bit [index preferred-bit coll]
+  (if (and (= (last (first coll)) (last (last coll))) (not= (first coll) (last coll)))
+    preferred-bit
+    (first (nth coll index (first coll)))))
+
+(defn get-frequent-bits-by-frequency-index [index preferred-bit frequencies]
+  (mapv #(find-bit index preferred-bit %) frequencies))
 
 (defn binary->int [string]
   (Integer/parseInt (apply str string) 2))
 
 (defn part-1 [file-content]
   (let [input (read-input file-content)
-        bits-by-index (for [i (range (count (first input)))] (get-column i input))
-        sorted-frequencies (mapv #(sort-by val (frequencies %)) bits-by-index)
-        gamma (get-relevant-bits 1 sorted-frequencies)
-        epsilon (get-relevant-bits 0 sorted-frequencies)]
+        sorted-frequencies (sorted-bit-frequencies input)
+        gamma (get-frequent-bits-by-frequency-index 1 1 sorted-frequencies)
+        epsilon (get-frequent-bits-by-frequency-index 0 0 sorted-frequencies)]
     (* (binary->int gamma) (binary->int epsilon))))
 
+(defn filter-binary [bit index coll]
+  (filter #(= bit (nth % index)) coll))
+
+(defn filter-input-by-frequency-index [index input]
+  (loop [i 0
+         coll input]
+    (if (= i (count (first input)))
+      coll
+      (recur (inc i) (filter-binary (nth (get-frequent-bits-by-frequency-index index index (sorted-bit-frequencies coll)) i) i coll)))))
+
+(defn part-2 [file-content]
+  (let [input (read-input file-content)
+        oxygen-generator-rating (->> input (filter-input-by-frequency-index 1) first binary->int)
+        co2-scrubber-rating (->> input (filter-input-by-frequency-index 0) first binary->int)]
+    (* oxygen-generator-rating co2-scrubber-rating)))
+
 (defn -main [& args]
-  (println "Part 1:" (part-1 (slurp (first args)))))
+  (println "Part 1:" (-> args first slurp part-1))
+  (println "Part 2:" (-> args first slurp part-2)))
